b3dee70e0bff84d984362c8ff5d279234249c863
Danila Smirnov <ylptred@gmail.com>
2022-10-12 19:59:18 +0300

    feat: added Advanced scenario

--- src/test/scala/load/LoadScript.scala
+++ src/test/scala/load/LoadScript.scala
@@ -6,8 +6,10 @@ import scala.concurrent.duration._
 import java.util.concurrent.ThreadLocalRandom
 
 class LoadScript extends Simulation {
-  val search_feeder = csv("search.csv").random
+  val search_feeder = csv("search.csv").random // выбирает рандомную строчку из таблицы с данными для поиска
 
+
+  // Процесс поиска: Домашняя страница -> Поиск -> Уточненный поиск
   val search =
     exec(http("Home").get("/"))
       .pause(1)
@@ -21,6 +23,7 @@ class LoadScript extends Simulation {
       .pause(1)
 
 
+  // Список разделов(страниц) сайта
   val pages = List(
     "/supermarket",
     "/landing/megarasprodazha",
@@ -29,14 +32,16 @@ class LoadScript extends Simulation {
     "/multicart"
   )
 
+  // Браузинг сайта (посещение страниц из списка)
   val browse =
     repeat(3, "i") {
       exec(http(s"Page $pages(#{i})").get(s"$pages(#{i})")).pause(1)
     }
 
 
-  val auth_feeder = csv("auth.csv").random
+  val auth_feeder = csv("auth.csv").random // выбор рандомных строк из таблицы с аутентификационными данными продавцов
 
+  // Процесс аутентификации продавца: посещение страницы аутентификации и ввод данных
   val seller_auth =
     tryMax(2) {
       exec(http("Auth Home").get("/auth"))
@@ -56,6 +61,8 @@ class LoadScript extends Simulation {
     }
       .exitHereIfFailed
 
+
+  // конфиг http для пользователей
   val httpUser = http
     .baseURL("https://sbermegamarket.ru/")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
@@ -64,6 +71,7 @@ class LoadScript extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
+  // конфиг http для продавцов
   val httpSeller = http
     .baseURL("https://partner.sbermegamarket.ru/")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
@@ -72,9 +80,11 @@ class LoadScript extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
+  // Создание и запуск сценариев
   val users = scenario("Users").exec(search, browse)
   val sellers = scenario("Seller").exec(search, browse, seller_auth)
 
+  // 1000 пользователей для каждого сценария на протяжении 10 минут
   setUp(
 
     users.inject(rampUsers(1000) over (10 minutes))
